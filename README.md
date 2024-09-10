a91f781fc7e0a139115d1bf79c7d2a6ab5dfd627
grizzly <emilnissen2@gmail.com>
2024-09-10 22:08:55 +0200

    Fixed the ui and inpuit buffering issue. Added readme.

---
+++ README.md
@@ -0,0 +1,9 @@
+# To compile the program:
+In your terminal navigate to the folder containing the source code and use the commands:
+```sh
+gcc -o caesar caesar.c cipher.c
+```
+# To run the program:
+```sh
+./caesar
+```
--- caesar.c
+++ caesar.c
@@ -4,35 +4,51 @@
 
 
 int main(){
+    printf("----------------------------\n");
+    printf("Ave Caesar, morituri te salutant!\n");
+    printf("Caesar cipher\n");
 
+
+    char input[255];
+    int shiftvalue;
+    
     while(1){
+        printf("----------------------------\n");   
+        printf("Choose an option\n");
+        printf("1. Encrypt\n");
+        printf("2. Decrypt\n");
+        printf("q. Quit\n");
+        printf("----------------------------\n");
         char choice = get_choice();
-        char input[255];
-        int shiftvalue;
+
         switch (choice)
         {
         case 'q':
             return 0;
             break;
         case '1':
+            getchar(); // Remove newline from buffer
             printf("----------------------------\n");
             printf("Enter a string to encrypt\n");
             printf("----------------------------\n");
-            fgets(input, 255, stdin);
+            fgets(input, 255, stdin); // Read input from user. Basically better gets.
             printf("Enter a shift value: ");
             scanf("%d", &shiftvalue);
             caesar_encrypt(input, shiftvalue);
-            printf("Encrypted string: %s\n", input);
+            printf("Encrypted string: \033[4m%s\033[0m\n", input);
+            getchar();
             break;
         case '2':
-             printf("----------------------------\n");
+            getchar();
+            printf("----------------------------\n");
             printf("Enter a string to decrypt\n");
             printf("----------------------------\n");
             fgets(input, 255, stdin);
             printf("Enter a shift value: ");
             scanf("%d", &shiftvalue);
             caesar_decrypt(input, shiftvalue);
-            printf("Decrypted string: %s\n", input);
+            printf("Decrypted string: \033[4m%s\033[0m\n", input);
+            getchar();
             break;
         default:
             break;
